398c8531c866dd560f1dde8a19f2e053ac287b19
Robert <roberttemta@gmail.com>
2024-07-03 14:26:09 -0500

    modification

--- bancked.tf
+++ bancked.tf
@@ -5,7 +5,7 @@ terraform {
     key            = "dev/terrform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "utc-app1-locktable"
+    //dynamodb_table = "utc-app1-locktable"
     //dynamodb_table = "utc-app2-lock-table"
     //dynamodb_table = "utc-app2-lock-table"
 
