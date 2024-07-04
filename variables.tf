42c5021de55e9e577a71a5393e0e9328d16b974d
Robert <roberttemta@gmail.com>
2024-07-04 00:07:17 -0500

    new commit

--- variables.tf
+++ variables.tf
@@ -2,14 +2,14 @@
 # Varaibles creation
 
 variable "REGION" {
-  
+  default = "us-east-1"
 }
 
 variable "INSTANCE_TYPE" {
-
+    default = "t2.micro"
 }
 
 variable "ENVIRONMENT" {
-  
+  default = "dev"
 }
 
